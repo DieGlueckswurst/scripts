7a79241313dc0e3e1306b75d78a846ad42fd2ce1
DieGlueckswurst <chriskonnerth@gmx.de>
2025-01-20 10:39:18 +0100

    Add lit alias for cleaning, getting, and building Flutter app

--- flutter/aliases.sh
+++ flutter/aliases.sh
@@ -1,7 +1,7 @@
 alias fb="fvm flutter pub run build_runner clean && fvm dart run build_runner build --delete-conflicting-outputs"
 alias fg="fvm flutter pub get"
 alias fc="fvm flutter clean && rm -rf ~/Library/Developer/Xcode/DerivedData && fvm flutter pub get && rm -rf ./build/app/outputs/apk"
-
+alias lit="fc && fg && fb"
 
 
 
